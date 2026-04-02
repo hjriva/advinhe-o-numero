c1d285c9af5e40a6f0de15d5f8eb83f7e3daba27
hjriva <157857573+hjriva@users.noreply.github.com>
2026-04-02 12:18:36 -0300

    jogo em csharp

---
+++ advinheonumero csharp/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Número a ser advinhado!");
+string NumAlvo = Console.ReadLine();
+Console.WriteLine("Número salvo. Aguarde...");
+Console.Clear();
+
+Console.WriteLine($"O número alvo tem {NumAlvo.Length} caracteres. Tente advinhar:");
+
+string NumAdv = Console.ReadLine();
+
+
+while (NumAlvo != NumAdv) {
+int PosCor = 0;
+ for (int i=0; i <= (NumAlvo.Length - 1); i++)
+    {
+        if (NumAdv[i] == NumAlvo[i])
+    {
+        PosCor++;
+    } } 
+    Console.WriteLine($"{PosCor} posições corretas. Tente novamente:");
+    NumAdv = Console.ReadLine();
+}
+
+Console.WriteLine($"Você acertou! O número correto é {NumAlvo}");
+
+
+
+
